15190ccee5922967e0c2921da8b5316835008cb9
cedricdsc <cedric.dsc@hotmail.de>
2021-05-04 11:57:39 +0200

    Post request mit json via button

--- website/scripts.js
+++ website/scripts.js
@@ -1,16 +1,30 @@
-/* Client */
+/* CLIENT */
 var databaseName = "food";
 var replicaSetPorts = ["27017","27027","27037","27047","27057","27067"];
 var routerPort = ["27019"];
 
-/*function httpPost(theUrl){
-    console.log("Sending POST");
-    var urlWithParams = theUrl + "/?replicaSetPorts=123";
-    console.log(urlWithParams);
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "POST", theUrl, true ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+/* POST WITH JSON */
+var xhr = new XMLHttpRequest();
+var url = "http://localhost:3000/checkReplicaSetsStatus";
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json);
+    }
+};
 
-httpPost("/checkConnection");*/
+/* DATA */
+var data = {   
+    "replicaSetPorts": [
+        "27017", "27027","27037","27047","27057","27067"
+    ],
+    "databaseName" : "food"
+}
+var dataJSON = JSON.stringify(data);
+
+/* SEND */
+document.getElementById('btn').addEventListener("click", function(){
+    xhr.send(dataJSON);
+});
