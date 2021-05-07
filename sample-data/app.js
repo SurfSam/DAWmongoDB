0de2117fbbc83fefd99c00be46c747ae973d1d76
Sam <35116088+SurfSam@users.noreply.github.com>
2021-05-07 11:01:53 +0200

    added fruit sample data

---
+++ sample-data/app.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+
+const FRUIT_TYPES = [
+    {
+        type: "🍏",
+        color: "green",
+        price: "$0.20",
+        skBase: "ap"
+    },
+    {
+        type: "🍌",
+        color: "yellow",
+        price: "$0.25",
+        skBase: "ba"
+    },
+    {
+        type: "🍓",
+        color: "red",
+        price: "$0.40",
+        skBase: "st"
+    },
+    {
+        type: "🍑",
+        color: "orange",
+        price: "$0.50",
+        skBase: "pe"
+    },
+    {
+        type: "🍍",
+        color: "yellow",
+        price: "$2.00",
+        skBase: "pi"
+    }
+];
+
+var resultData = [];
+var type_count = {};
+
+initTypeCounts(type_count);
+
+for (let i = 0; i < 1000; i++) {
+    let newFruit = {...FRUIT_TYPES[getRandomInt(FRUIT_TYPES.length)]};
+
+    newFruit.fruit_id = `${newFruit.skBase}${++type_count[newFruit.type]}`;
+    delete newFruit.skBase;
+
+    resultData.push(newFruit);
+}
+
+let jsonData = JSON.stringify(resultData);
+
+fs.writeFileSync(path.join(__dirname, '/data/fruitExample.json'), jsonData);
+
+function initTypeCounts(dict) {
+    FRUIT_TYPES.forEach(fruit => dict[fruit.type] = 0);
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
